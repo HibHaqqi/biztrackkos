471cca35cd747e2a04871b8e6aac62aafb88f7e7
Hibhaqqi <haqqi09@gmail.com>
2025-07-11 23:12:12 +0700

    login issue

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -3,11 +3,8 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { createSession } from '@/lib/session';
-
-const FAKE_USER = {
-  email: 'admin@example.com',
-  password: 'password',
-};
+import prisma from '@/lib/db';
+import bcrypt from 'bcryptjs';
 
 export async function login(
   prevState: { error: string | null },
@@ -16,7 +13,11 @@ export async function login(
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
 
-  if (email === FAKE_USER.email && password === FAKE_USER.password) {
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
     await createSession();
     // Redirect must be called outside of a try/catch block.
     redirect('/');
@@ -26,6 +27,7 @@ export async function login(
 }
 
 export async function logout() {
-  cookies().set('session', '', { expires: new Date(0) });
+  const cookieStore = cookies();
+  cookieStore.set('session', '', { expires: new Date(0) });
   redirect('/login');
 }
